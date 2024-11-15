b53762f441176b67ad76c548baab4c0bd2f0a335
Abdurrahman <beketabdurrahman1950@gmail.com>
2024-11-15 13:19:38 +0500

    стелды

--- day-4/script.js
+++ day-4/script.js
@@ -1,21 +1,27 @@
-function calculateAverageGrade() {
-    // Студенттер санын енгізіңіз
-    let studentCount = Number(prompt("Студенттер санын енгізіңіз: "));
+// let san = +prompt("Student sanun engyz")
+// let i=1;
+// let TotalGrade = 0;
 
-    // Бағаларды енгізу
-    let grades = [];
-    for (let i = 0; i < studentCount; i++) {
-        let grade = Number(prompt(`${i + 1}-студенттің бағасын енгізіңіз: `));
-        grades.push(grade);
+// while (i<=san) {
+//     let baga = +prompt(i +"studentin bagasy")
+//     TotalGrade = TotalGrade + baga;
+//     i++;
+// }
+// let ortalama_baga = TotalGrade / san
+// alert("Studenterdin ortalama bagasynin mani: "+ ortalama_baga)
+
+
+
+
+
+for (let number = 1; number <= 20; number++) {
+    if (number % 3 === 0 && number % 5 === 0) {
+        alert(number + " буз");
+    } else if (number % 3 === 0) {
+        alert(number + " фис");
+    } else if (number % 5 === 0) {
+        alert(number + " гуз");
+    } else {
+        alert(number);
     }
-
-    // Орташа мәнін есептеу
-    let sum = grades.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-    let averageGrade = sum / studentCount;
-
-    // Нәтижені шығару
-    alert(`Студенттердің бағаларының орташа мәні: ${averageGrade.toFixed(2)}`);
 }
-
-// Функцияны шақыру
-calculateAverageGrade();
---
+++ day-4hw/script.js
@@ -0,0 +1,26 @@
+function findMinMaxDivisibleByFive(userNumber) {
+    let min = null;
+    let max = null;
+
+    for (let number = 1; number <= userNumber; number++) {
+        if (number % 5 === 0) {
+            if (min === null || number < min) {
+                min = number;
+            }
+            if (max === null || number > max) {
+                max = number;
+            }
+        }
+    }
+
+    return {min, max};
+}
+
+let userNumber = parseInt(prompt("Кез келген оң санды енгізіңіз:"));
+let result = findMinMaxDivisibleByFive(userNumber);
+
+if (result.min !== null && result.max !== null) {
+    alert("Ең кіші сан: " + result.min + "\nЕң үлкен сан: " + result.max);
+} else {
+    alert("5-ке бөлінетін сандар жоқ.");
+}
